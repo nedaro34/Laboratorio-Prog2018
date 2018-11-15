f4de65194aa3b2e5ad90d18ff8e1beff15930db8
lucas zarate <lucasmaxzarate@gmail.com>
2018-11-15 12:53:20 -0300

    Merge branch 'master' of https://github.com/nedaro34/Laboratorio-Prog2018

--- src/principal/CentroClinicaMedica.java
+++ src/principal/CentroClinicaMedica.java
@@ -6,11 +6,14 @@ import javax.swing.JOptionPane;
 
 
 public class CentroClinicaMedica {
-    
+    //Esta clase va a contener todas las listas enlazadas para que tome la vriables
     //creacion de listas
     
     private static LinkedList<Afiliados> afiliado = new LinkedList<Afiliados>();
-    
+    private static LinkedList<Choferes> chofer = new LinkedList<Choferes>();
+    private static LinkedList<Doctores> doctor = new LinkedList<Doctores>();
+    private static LinkedList<Enfermeros> enfermero = new LinkedList<Enfermeros>();
+
     //metodos afiliados
     //igreso de afiliado a las listas
     
@@ -28,6 +31,40 @@ public class CentroClinicaMedica {
         }
 
             afiliado.add(afiliados);
+           
+    }
+
+      public void Ingresar_Doctores(Doctores doctores,int dni)throws AfiliadosDIgualExcepcion
+    {
+        if(chofer.size()>0)
+        {
+            for(int i=0;i<chofer.size();i++)
+            {
+                if(chofer.get(i).getDocumento()==dni)
+                {
+                    throw new AfiliadosDIgualExcepcion();
+                }
+            } 
+        }
+
+            doctor.add(doctores);
+           
+    }
+      
+         public void Ingresar_Choferes(Choferes chofer1,int dni)throws AfiliadosDIgualExcepcion
+    {
+        if(chofer.size()>0)
+        {
+            for(int i=0;i<chofer.size();i++)
+            {
+                if(chofer.get(i).getDocumento()==dni)
+                {
+                    throw new AfiliadosDIgualExcepcion();
+                }
+            } 
+        }
+
+            chofer.add(chofer1);
            
     }
     
@@ -51,5 +88,77 @@ public class CentroClinicaMedica {
         }    
         return null;
     } 
+
+    public Choferes Buscar_Chofer(int dni) {
+        if(chofer.size()>0)
+        {
+            for(int i=0;i<chofer.size();i++)
+            {
+                if(chofer.get(i).getDocumento()==dni)
+                {
+                    return chofer.get(i);
+                }
+            }
+            JOptionPane.showMessageDialog(null,"El chofer no existe");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"Lista de afiliado vasia");
+        }    
+        return null;   
+    }
+
+    public Doctores Buscar_Doctor(int dni) {
+ if(chofer.size()>0)
+        {
+            for(int i=0;i<doctor.size();i++)
+            {
+                if(doctor.get(i).getDocumento()==dni)
+                {
+                    return doctor.get(i);
+                }
+            }
+            JOptionPane.showMessageDialog(null,"El chofer no existe");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"Lista de afiliado vasia");
+        }    
+        return null;    }
+
+    public Enfermeros Buscar_Enfermero(int dni) {
+if(enfermero.size()>0)
+        {
+            for(int i=0;i<enfermero.size();i++)
+            {
+                if(enfermero.get(i).getDocumento()==dni)
+                {
+                    return enfermero.get(i);
+                }
+            }
+            JOptionPane.showMessageDialog(null,"El chofer no existe");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"Lista de afiliado vasia");
+        }    
+        return null;    }
+
+    public void Ingresar_Choferes(Enfermeros enfermero1, int dni)throws AfiliadosDIgualExcepcion
+            {
+ if(chofer.size()>0)
+        {
+            for(int i=0;i<chofer.size();i++)
+            {
+                if(chofer.get(i).getDocumento()==dni)
+                {
+                    throw new AfiliadosDIgualExcepcion();
+                }
+            } 
+        }
+
+            enfermero.add(enfermero1);  
+            }
     
 }
+
